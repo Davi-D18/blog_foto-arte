f2ac35c723742342cbe3b339e65c4536a97fcae9
Davi-D18 <luiz.araujo@alunomicrolins.com.br>
2024-11-26 18:50:15 -0300

    Novo álbum adicionado

--- src/data/albuns.js
+++ src/data/albuns.js
@@ -2,7 +2,7 @@ export const albuns = [
   {
     titulo: "Praia",
     autor: "Luiz Davi",
-    descricao: "Uma coleção de fotos tiradas nas praias mais bonitas.",
+    descricao: "Uma coleção de fotos tiradas nas praias.",
     imagens: [
       "https://lh3.googleusercontent.com/pw/AP1GczNFOWLsYma2-8pLyprHuXZ0YXFstALCVqajgpG6xyq9UhPongj5pNj3g9Hn_UTJ08w3SGGCKJuFrztXGmDOSRl5e93wSyLPirEfps5XB7FehAzk-Oeg31lUu4Uq-OY4FBVZaDc5fFDew9UjiF9pUQEBAA=w590-h787-s-no-gm?authuser=0",
       "https://lh3.googleusercontent.com/pw/AP1GczO7Vq3LekdZuINOWa-94ydtArOJSP0eSbVnODBzuOnNQBlqb48-8efIyIf6A5Aext13qnNtb5bDggY0B8vAXNlOBYP63tDpEyIMrsdllpDP5Q5usEsbJAsHlrIQmj9KhSU7aczkd-n4787g9usoM44trQ=w590-h787-s-no-gm?authuser=0",
@@ -13,7 +13,7 @@ export const albuns = [
     titulo: "Salvador",
     autor: "Irla Andrade",
     descricao:
-      "Fotos de uma visita técnica para Salvador com a turma do curso de Turismo",
+      "Fotos de uma visita técnica para Salvador com a turma do curso de Turismo.",
     imagens: [
       "https://lh3.googleusercontent.com/pw/AP1GczNn1N7TL2xvrhd6YX5LeNMOHhMY_bJClJ3yvR_BeNvUdgH0XKkCtTH3cMOxseTuDnjHly0nBZYk9Z28f8n73lxv2PgOzUcgLGojtoIZdv_5Ftk5HRSEU6rD40rbvhWaw9QfSoagmP-E7Gl8WPvKnxJl=w512-h911-s-no-gm",
       "https://lh3.googleusercontent.com/pw/AP1GczNieHk17Qxg-6ZDqSsGhyhM-hRtJ_uxRxiFFwNADOgdcZP4Mk-Vgfo1GUNTwVxAbFReCj8dj1K3BQuhLp9Onx_60rHzoVodCAbGQG0iI1QF4q1GHyiD_rkaUcCzF8Z6c8TxIbNl5aKOLw52vDF_tEht=w683-h911-s-no-gm",
@@ -30,5 +30,15 @@ export const albuns = [
       "https://lh3.googleusercontent.com/pw/AP1GczM7NevRor1nj6q_sPzTuACTe3ZGjTxe9mlItqdZnRE8BJEdD8tiUHpESus-8l-WK7ZbxEMnZjF5Uo02GB8KVrnXIbM1pg-FnK9TVfGK4zMOTUsLo9qbEGxBv7mh51WXfXlk800dFG-A9jsi_AxwwCRkEA=w688-h911-s-no-gm",
     ],
     link: "https://photos.app.goo.gl/bfm6JQxRXFpjkvV69",
+  },
+  {
+    titulo: "Ilha Grande",
+    autor: "Deise Santos",
+    descricao: "Algumas fotos tiradas em uma das mais lindas ilhas.",
+    imagens: [
+      "https://lh3.googleusercontent.com/pw/AP1GczNB-jA6Z0Rf8zMOQ9thCy6Pu2zdC5cXeSs7CXcI4BmfsuoV-xYzE27QnYcxWC-8xnn1kdMJO1QGFhal4BNzrLWt6J-PI2BrZgkyDx9sj-PGQwreac186dG8fSFWPROMLYvl5Vpe5M9bSNuk_6YMYTQ=w512-h911-s-no-gm",
+      "https://lh3.googleusercontent.com/pw/AP1GczMAgigL_WRGArWZzD4rKjlr6O8ym0PRV-BKs5okxCF5fF3oHRW5-2RSJeeJsMINzVs_UxaakMotdKUjWMoMtPIErt7DbblWgCsj9j6sjl9Udo-55ajGgjEch09YF3-L6PfpSVKIruGh8-wCJSUmJrk=w512-h911-s-no-gm",
+    ],
+    link: "https://photos.app.goo.gl/JqgFFvVYnxEKAoGF8",
   },
 ];
